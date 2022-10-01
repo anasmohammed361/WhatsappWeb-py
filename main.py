61c5b5416f22cb88d21b2267a7ac51bcf5dcf4e3
Mohammed Anas <78010612+anasmohammed361@users.noreply.github.com>
2022-10-01 14:47:53 +0530

    Altered main .py

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from Whats import Whats
 
 
 app=Whats()
-app.send_doc("+91 74484 51031","/home/anasmohammed361/Pictures/Screenshots/Screenshot_2022-07-05-11-18-41_1680x1050.png",is_img=True)
+#app.send_msg()
+#app.send_doc()
